8b7763a1d7f4004ea9b587c9577b082d38175dd0
Daniel Standage <daniel.standage@nbacc.dhs.gov>
2019-08-14 12:25:38 -0400

    Update Rfam covariance models to 14.1

---
+++ db/cm/__build/archaea.sql
@@ -0,0 +1,21 @@
+SELECT DISTINCT f.rfam_acc, f.type, f.description
+FROM taxonomy tx
+INNER JOIN rfamseq rf ON rf.ncbi_id = tx.ncbi_id
+INNER JOIN full_region fr ON fr.rfamseq_acc = rf.rfamseq_acc
+INNER JOIN family f ON f.rfam_acc = fr.rfam_acc
+WHERE ((f.type LIKE 'Gene;' AND f.description NOT LIKE '%transfer-messenger RNA')
+OR f.type LIKE '%CRISPR;'
+OR f.type LIKE '%antisense;'
+OR f.type LIKE '%antitoxin;'
+OR f.type LIKE '%miRNA;'
+OR f.type LIKE '%ribozyme;'
+OR f.type LIKE '%sRNA;'
+OR f.type LIKE '%snRNA%'
+OR f.type LIKE 'Intron;'
+OR f.type LIKE 'Cis-reg;'
+OR f.type LIKE '%IRES;'
+OR f.type LIKE '%frameshift_element;'
+OR f.type LIKE '%leader;'
+OR f.type LIKE '%riboswitch;'
+OR f.type LIKE '%thermoregulator;')
+AND tx.tax_string LIKE 'Archaea%';
