20b2b5b8453c9e0fc912cf01f8cdef9ac5cc9a2b
Torsten Seemann <torsten.seemann@gmail.com>
2025-12-14 10:07:15 +1100

    Database upgrade pipeline

---
+++ update-dbs/AMRprot-to-AMR.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/env perl
+use strict;
+
+#0 >WP_002791310.1|   
+#1 1|
+#2 B1|
+#3 blaOXA-489|
+#4 blaOXA-61_fam|
+#5 hydrolase|
+#6 2|
+#7 BETA-LACTAM|
+#8 BETA-LACTAM|
+#9 OXA-61_family_class_D_beta-lactamase_OXA-489 
+
+#>NG_049762.1
+#~~~blaOXA-48
+#~~~carbapenem-hydrolyzing class D beta-lactamase OXA-48
+#~~~
+
+while (<>) {
+  if (m/^>(.*)$/) {
+    my $hdr = $1;
+    chomp $hdr;
+    my @f = split m/\|/, $hdr;
+    $f[9] =~ s/_/ /g;
+    print 
+      ">",
+      join('~~~',
+        $f[0],
+        $f[3],
+        $f[9],
+        '',
+      ),
+      "\n";
+  }
+  else {
+    print $_;
+  }
+}
